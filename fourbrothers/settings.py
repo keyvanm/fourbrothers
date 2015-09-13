5244719767648a70dc4631fa2f5cdce80a695e11
Keyvan Mosharraf <keyvanm@icloud.com>
2015-09-13 15:51:04 -0400

    Added local settings

--- fourbrothers/settings.py
+++ fourbrothers/settings.py
@@ -19,11 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n%5e+b!u=j(u6td(xq3rhl#n&2_n*szb%$8u4cyk=v*h7ap&!f'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+if 'DJANGO_DEBUG' in os.environ:
+    DEBUG = (os.environ['DJANGO_DEBUG'] == "True")
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #
+    'user_manager',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -74,12 +75,17 @@ WSGI_APPLICATION = 'fourbrothers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
 
 
 # Internationalization
@@ -87,7 +93,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Toronto'
 
 USE_I18N = True
 
@@ -100,3 +106,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'indie_static'),
+)
+
+# Secret key for prod server
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# local settings
+try:
+    from local_settings import *
+except ImportError, e:
+    pass
+
+try:
+    assert SECRET_KEY
+except (NameError, AssertionError):
+    from secret_key_gen import SECRET_KEY
