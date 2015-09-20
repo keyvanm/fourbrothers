f1233481898c3a11064f7fe82a1ff2fd15e225c2
Keyvan Mosharraf <keyvanm@icloud.com>
2015-09-20 14:17:41 -0400

    Sign up

--- user_manager/forms.py
+++ user_manager/forms.py
@@ -25,3 +25,23 @@ class CarForm(ModelForm):
     class Meta:
         model = Car
         exclude = ['owner', 'deleted']
+
+
+class MySignupForm(forms.Form):
+    first_name = forms.CharField(max_length=40, required=True,
+                                 widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+    last_name = forms.CharField(max_length=40, required=True,
+                                widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
+    phone_number = forms.CharField(max_length=40, required=True,
+                                widget=forms.TextInput(attrs={'placeholder': 'Phone number'}))
+
+    def signup(self, request, user):
+        form = MySignupForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            user.first_name = form.cleaned_data['first_name']
+            user.last_name = form.cleaned_data['last_name']
+            user.profile.phone_number = form.cleaned_data['phone_number']
+            user.profile.save()
+            user.save()
+
