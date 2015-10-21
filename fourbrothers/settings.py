13454f43284b1620f90a8e15cf041467e691b999
Keyvan Mosharraf <keyvanm@icloud.com>
2015-10-21 01:28:16 -0400

    Send mail was causing problems. Will leave the settings to default

--- fourbrothers/settings.py
+++ fourbrothers/settings.py
@@ -173,15 +173,6 @@ else:
 # SESSION_COOKIE_SECURE = True
 # CSRF_COOKIE_SECURE = True
 
-
-#Debug Email - MailCatcher
-if DEBUG:
-    EMAIL_HOST = '127.0.0.1'
-    EMAIL_HOST_USER = ''
-    EMAIL_HOST_PASSWORD = ''
-    EMAIL_PORT = 1025
-    EMAIL_USE_TLS = False
-
 # Email
 if 'EMAIL_HOST_USER' in os.environ:
     EMAIL_USE_TLS = True
