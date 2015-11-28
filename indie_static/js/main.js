000c8161a9b84d2ff3005d809f83df3c30d42cb6
Jeffrey Gingras <jeffreygingras@Jeffreys-MacBook-Pro.local>
2015-11-28 17:41:50 -0500

    css

--- indie_static/js/main.js
+++ indie_static/js/main.js
@@ -99,3 +99,21 @@ $(document).ready(function animationHover(element, animation){
 });
 
 
+    $(document).ready(function () {
+
+        // you want to enable the pointer events only on click;
+
+        $('#map_canvas1').addClass('scrolloff'); // set the pointer events to none on doc ready
+        $('#googlemaps').on('click', function () {
+            $('#map_canvas1').removeClass('scrolloff'); // set the pointer events true on click
+        });
+
+        // you want to disable pointer events when the mouse leave the canvas area;
+
+        $("#map_canvas1").mouseleave(function () {
+            $('#map_canvas1').addClass('scrolloff'); // set the pointer events to none when mouse leaves the map area
+        });
+    });
+
+
+
