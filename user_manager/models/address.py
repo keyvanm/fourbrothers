4df4645a032bdf8dc4db4b70145f64800af89397
Keyvan Mosharraf <keyvanm@icloud.com>
2015-12-05 04:02:49 -0500

    Made the default choice for province Ontario

--- user_manager/models/address.py
+++ user_manager/models/address.py
@@ -10,7 +10,7 @@ class Address(TimeStampedModel):
     CITY_CHOICES = Choices(('Toronto', 'Downtown Toronto'), 'East York', 'Etobicoke', 'Markham', 'North Toronto', 'North York', 'Scarborough', 'Thornhill', 'Vaughan', 'York')
     city = models.CharField(choices=CITY_CHOICES, max_length=200)
     STATE_CHOICES = Choices('Ontario')
-    state = models.CharField(max_length=200, verbose_name='Province', choices=STATE_CHOICES)
+    state = models.CharField(max_length=200, verbose_name='Province', choices=STATE_CHOICES, default='Ontario')
     postal_code = models.CharField(max_length=20, verbose_name="ZIP/Postal Code")
     country = models.CharField(max_length=200, default="Canada")
 
