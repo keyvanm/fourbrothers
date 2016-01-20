151195d23619788963949753290746be988e36e2
Jeffrey Gingras <jeffreygingras@Jeffreys-MacBook-Pro.local>
2016-01-19 19:34:30 -0500

    email fix

--- dashboard/views.py
+++ dashboard/views.py
@@ -84,8 +84,8 @@ class ApptComplete(LoginRequiredMixin, View):
     def post(self, request, pk):
         appt = get_object_or_404(Appointment, pk=pk)
         if settings.SEND_MAIL:
-            msg_plain = render_to_string('appt_mgmt/completion-email.txt', {'appt': appt})
-            msg_html = render_to_string('appt_mgmt/completion-email.html', {'appt': appt})
+            msg_plain = render_to_string('appt_mgmt/completion-email.txt', {'appt': self.appointment})
+            msg_html = render_to_string('appt_mgmt/completion-email.html', {'appt': self.appointment})
 
             subject, from_email, to = 'Appointment Completion', 'info@fourbrothers.com', self.appointment.user.email
 
