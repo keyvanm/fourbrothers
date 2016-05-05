86fad6220de5204beb1c61d5f21b8f0b995293f2
Keyvan Mosharraf <keyvanm@icloud.com>
2016-05-05 12:13:43 -0400

    Testing

--- appt_mgmt/invoice_views.py
+++ appt_mgmt/invoice_views.py
@@ -33,13 +33,13 @@ def create_and_charge_new_customer(request, token, total_price):
     if not setting.DEBUG:
         request.user.profile.stripe_customer_id = customer.stripe_id
         request.user.profile.save()
+    raise Http404
     stripe.Charge.create(
         amount=int(total_price * 100),  # amount in cents, again
         currency="cad",
         customer=customer.id,
         description="Paid ${} for service".format(total_price)
     )
-    raise Http404
 
 
 class InvoiceCreateView(LoginRequiredMixin, CreateView):
